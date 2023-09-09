514dc41c280c71f2b76da0ef41308a2d418e2525
OddlyShapedFn <95105454+oddlyshapedfn@users.noreply.github.com>
2023-09-08 23:55:01 -1000

    Cleanup main

--- create_video.py
+++ create_video.py
@@ -3,6 +3,13 @@ import torch
 import cv2
 import kornia.geometry as kgeom
 
+IN_NAME='tdw.m4v'
+OUT_NAME='out.mp4'
+WIDTH=1280
+HEIGHT=720
+FPS=60
+DECIMATE=2
+
 def get_displacements(preds, thresh=0.5, device='cuda'):
     pts = preds[0].keypoints.data.clone()
     dims = preds[0].orig_shape
@@ -29,27 +36,33 @@ def shift(img, displacements, device='cpu'):
         outs.append(shifted)
     return torch.stack(outs).squeeze(1)
 
-model = U.YOLO('yolov8s-pose.pt')
+if __name__ == '__main__':
+    model = U.YOLO('yolov8s-pose.pt')
 
-boxsz = 512
-fname = 'out.mp4' # (752, 1408)
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-writer = cv2.VideoWriter(fname, fourcc, 30, (1408, 752))
+    fname = OUT_NAME
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    writer = cv2.VideoWriter(fname, fourcc, FPS, (WIDTH, HEIGHT))
 
-reader = cv2.VideoCapture('vid.m4v')
-while writer.isOpened():
-    ret, frame = reader.read()
-    if frame is None:
-        break
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    preds = model(frame)[0]
-    if preds.keypoints.data.shape[1] == 0:
-        continue
-    v, d = get_displacements(preds)
-    shifted = shift(frame, d)
-    frame = shifted[0].permute(1,2,0).to(torch.uint8).cpu().numpy()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    writer.write(frame)
+    frame_count = 0
+    reader = cv2.VideoCapture(IN_NAME)
+    while writer.isOpened():
+        print(frame_count)
+        frame_count += 1
+        ret, frame = reader.read()
+        if frame is None:
+            break
+        if frame_count % DECIMATE != 0:
+            continue
 
-writer.release()
-reader.release()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        preds = model(frame)[0]
+        if preds.keypoints.data.shape[1] == 0:
+            continue
+        v, d = get_displacements(preds)
+        shifted = shift(frame, d)
+        frame = shifted[0].permute(1,2,0).to(torch.uint8).cpu().numpy()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        writer.write(frame)
+
+    writer.release()
+    reader.release()
